cc51747976b6b4f35aeb04845ca4f476658becc0
Esteban Madrigal <ememadrigal.em@gmail.com>
2022-01-18 16:14:37 -0600

    function module added, component deleted

--- client/function-store/src/app/app-routing.module.ts
+++ client/function-store/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'templates', loadChildren: () => import('./function/function.module').then(m => m.FunctionModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/function-store/src/app/function/function.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FunctionRoutingModule } from './function-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FunctionRoutingModule
+  ]
+})
+export class FunctionModule { }
