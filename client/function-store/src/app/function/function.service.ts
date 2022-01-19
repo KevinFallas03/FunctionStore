25cbfbf33e438a2719e921625f0d51853100d3fe
Esteban Madrigal <ememadrigal.em@gmail.com>
2022-01-18 18:26:08 -0600

    get functions from db done

--- client/function-store/src/app/function/function.service.ts
+++ client/function-store/src/app/function/function.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FunctionService {
+  _apiUrl = environment.url + "/api/functions";
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  post(new_function){
+    const headers = {"Content-Type":"application/json"};
+    return this._http.post<any>(this._apiUrl , new_function, {headers});
+  }
+  getAll(){
+    return this._http.get<any>(this._apiUrl);
+  }
+  getById(id){
+    return this._http.get<any>(this._apiUrl+'/'+id);
+  }
+  getManyById(data){
+    return this._http.get<any>(this._apiUrl+'/many/'+data);
+  }
+
+  findByIdAndUpdate(id, new_function){
+    return this._http.put(this._apiUrl+'/'+id, new_function);
+  }
+
+  deleteById(id){
+    return this._http.delete(this._apiUrl+'/'+id);
+  }
 }
--- client/function-store/src/app/function/searcher/searcher.component.ts
+++ client/function-store/src/app/function/searcher/searcher.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FunctionService } from '../function.service';
 @Component({
   selector: 'app-searcher',
   templateUrl: './searcher.component.html',
   styleUrls: ['./searcher.component.css']
 })
 export class SearcherComponent implements OnInit {
-
-  constructor() { }
+  all_functions:any = [{}];
+  constructor(
+    private _functionService: FunctionService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.get();
   }
-
+  get(){
+    this._functionService.getAll().subscribe(
+      data => {
+        this.all_functions = data
+      }
+    );
+  }
 }
